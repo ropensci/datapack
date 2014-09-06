ee251617a64ca5daaa402160ee9c84169675638d
Matt Jones <gitcode@magisa.org>
2014-09-06 14:55:42 -0800

    Initial copy of DataPackage and D1Object classes from DataONEorg/rdataone package.
    These classes are direct copies of the classes from the `rdataone` package, specifically from commit 681b5bbdc7facac133476c19855c5ed6220c2c1c.  They depend on rJava, which needs to be removed before this is useful.  This commit records the starting point before refactoring of the class to remove he java dependency was started.

---
+++ R/D1Object.R
@@ -0,0 +1,441 @@
+#
+#   This work was created by participants in the DataONE project, and is
+#   jointly copyrighted by participating institutions in DataONE. For
+#   more information on DataONE, see our web site at http://dataone.org.
+#
+#     Copyright 2011-2014
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+setClass("D1Object", representation(jD1o = "jobjRef") )
+
+#####################
+## D1Object constructors
+#####################
+
+## generic
+setGeneric("D1Object", function(...) { standardGeneric("D1Object")} )
+
+## no arguments in the signature
+## setMethod("D1Object", , function() {
+##   result <- new("D1Object")
+##   return(result)
+## })
+
+
+setMethod("initialize", "D1Object", function(.Object, id, data, format, mnNodeId) {
+
+  if (typeof(id) == "character") {
+    message("@@ D1Object-class:R initialize as character")
+    
+    ## build identifier to be used in system metadata
+    pid <- .jnew("org/dataone/service/types/v1/Identifier")
+    pid$setValue(id)
+    
+    ## Convert incoming data to byte array (byte[])
+    ioUtils <- .jnew("org/apache/commons/io/IOUtils") 
+    byteArray <- ioUtils$toByteArray(data)
+    
+    ## build the ObjectFormatIdentifier.
+    formatId <- .jnew("org/dataone/service/types/v1/ObjectFormatIdentifier")
+    formatId$setValue(format)
+    
+    
+    ## build a submitter Subject from the certificate
+    certman <- J("org/dataone/client/auth/CertificateManager")$getInstance()
+    cert <- certman$loadCertificate()
+    submitter <- .jnew("org/dataone/service/types/v1/Subject")
+    submitter$setValue(certman$getSubjectDN(cert))
+    
+    ## build the NodeReference
+    mnNodeRef <- .jnew("org/dataone/service/types/v1/NodeReference")
+    mnNodeRef$setValue(mnNodeId)
+    
+    jd1o <- .jnew("org/dataone/client/D1Object",
+                  pid, byteArray, formatId, submitter, mnNodeRef,
+                  check=FALSE)
+    
+    if (!is.null(e <- .jgetEx())) {
+      print("Java exception was raised")
+      print(.jcheck(silent=TRUE))
+      print(.jcheck(silent=TRUE))
+      print(e)
+      jd1o = .jnull("org/dataone/client/D1Object")
+    }
+  }
+  else {
+    message("@@ D1Object-class:R initialize as something else")
+    if (.jinstanceof(id,"org/dataone/client/D1Object")) {
+      message("@@ D1Object-class:R initialize with jobjRef")
+      jd1o <- id
+    }
+  }
+  
+  .Object@jD1o <- jd1o
+  return(.Object)
+})
+
+
+#########################################################
+### MNRead and MNStorage methods
+#########################################################
+
+## buildD1Object, implemented as a static way of constructing an object
+# TODO: Consider making this into a real constructor
+# Currently this is just a copy from the D1Client class, needs to be
+# refactored.
+#setGeneric("buildD1Object", function(id, data, format, mn_nodeid, ...) { 
+#  standardGeneric("buildD1Object")
+#})
+#
+#setMethod("buildD1Object",
+#          signature("character", "character", "character", "character"),
+#	  function(id, data, format, mn_nodeid) {
+#
+  # build identifier to be used in system metadata
+#  pid <- .jnew("org/dataone/service/types/v1/Identifier")
+#  pid$setValue(id)
+
+  # Set up/convert additional system metadata fields
+  # get the submitter from the certificate
+#  certman <- J("org/dataone/client/auth/CertificateManager")$getInstance()
+#  cert <- certman$loadCertificate()
+#  submitter <- .jnew("org/dataone/service/types/v1/Subject")
+#  submitter$setValue(certman$getSubjectDN(cert))
+
+  # Convert incoming data to byte array (byte[])
+#  ioUtils <- .jnew("org/apache/commons/io/IOUtils") 
+#  byteArray <- ioUtils$toByteArray(data)
+
+  # build the ObjectFormatIdentifier.
+#  format.id <- .jnew("org/dataone/service/types/v1/ObjectFormatIdentifier")
+#  format.id$setValue(format)
+
+  # build the NodeReference from the mn_nodeid.
+#  if(is.null(mn_nodeid) || (mn_nodeid == "")) {
+#    print("ERROR: A Member Node must be defined to create an object.")
+#    return(.jnull("org/dataone/client/D1Object"))
+#  }
+#  mn.noderef <- .jnew("org/dataone/service/types/v1/NodeReference")
+#  mn.noderef$setValue(mn_nodeid)
+
+  # Now build the object with the sysmeta values
+#  d1object <- .jnew("org/dataone/client/D1Object", pid, byteArray, format.id,
+#                    submitter, mn.noderef, check=FALSE)
+  #message("building object")
+#  if (!is.null(e <- .jgetEx())) {
+#    print("Java exception was raised")
+#    print(.jcheck(silent=TRUE))
+#    print(.jcheck(silent=TRUE))
+#    print(e)
+#  }
+
+#  return(d1object)
+#})
+
+#########################################################
+### Accessor methods
+#########################################################
+
+# Need accessors for sysmeta and data contained in the 
+# internal java D1Object instance
+
+#########################################################
+### Utility methods
+#########################################################
+
+
+#' Get the Contents of the Specified Data Object
+#' 
+#' @param x  D1Object or DataPackage: the data structure from where to get the data
+#' @param id Missing or character: if @code{x} is DataPackage, the identifier of the
+#' package member to get data from
+#' @param ... (not yet used)
+#' @returnType character 
+#' @return character representation of the data
+#' 
+#' @author rnahf
+#' @export
+setGeneric("getData", function(x, id, ...) {
+    standardGeneric("getData")
+})
+ 
+setMethod("getData", signature("D1Object"), function(x, fileName=NA) {
+	jD1Object = x@jD1o
+	if(!is.jnull(jD1Object)) {
+		databytes <- jD1Object$getData()
+		if(is.null(databytes)) {
+			print(paste("Didn't find data in:", id))
+			return()
+		}
+        
+        if (!is.na(fileName)) {
+            ## Write data to file
+            jFileOutputStream <- .jnew("java/io/FileOutputStream", fileName)
+            jInputStream <- .jnew("java/io/ByteArrayInputStream", databytes)
+            ioUtils <- .jnew("org/apache/commons/io/IOUtils")
+            ioUtils$copy(jInputStream, jFileOutputStream)
+            returnVal = fileName
+        } else {
+            ## return data as string
+            jDataString <- .jnew("java/lang/String",databytes,"UTF-8")
+            if (!is.null(e<-.jgetEx())) {
+                print("Java exception was raised")
+                print(e)
+                print(.jcheck(silent=FALSE))
+            }
+            dataString <- jDataString$toString()
+            returnVal = dataString
+        }
+        
+		return(returnVal)
+	}
+})
+
+
+
+#' Get the Identifier of the D1Object
+#' @param x D1Object
+#' @param ... (not yet used)
+#' @returnType character
+#' @return the identifier
+#' 
+#' @author rnahf
+#' @export
+setGeneric("getIdentifier", function(x, ...) {
+    standardGeneric("getIdentifier")
+})
+
+setMethod("getIdentifier", signature("D1Object"), function(x) {
+    jD1Object = x@jD1o
+	if(!is.jnull(jD1Object)) {
+		jPid <- jD1Object$getIdentifier()
+		if(is.null(jPid)) {
+			return()
+		}
+		return(jPid$getValue())
+	}
+})
+
+
+#' Get the FormatId of the D1Object
+#' @param x D1Object
+#' @param ... (not yet used)
+#' @returnType character
+#' @return the formatId
+#' 
+#' @author rnahf
+#' @export
+setGeneric("getFormatId", function(x, ...) {
+			standardGeneric("getFormatId")
+		})
+
+setMethod("getFormatId", signature("D1Object"), function(x) {
+			jD1Object = x@jD1o
+			if(!is.jnull(jD1Object)) {
+				jFormatId <- jD1Object$getFormatId()
+				if(is.null(jFormatId)) {
+					return()
+				}
+				return(jFormatId$getValue())
+			}
+		})
+
+
+#' Add a Rule to the AccessPolicy to Make the Object Publicly Readable
+#' 
+#' To be called prior to creating the object in DataONE.  When called before 
+#' creating the object, adds a rule to the access policy that makes this object
+#' publicly readable.  If called after creation, it will only change the system
+#' metadata locally, and will not have any affect. 
+#' @param x D1Object
+#' @param ... (not yet used)
+#' @returnType NULL
+#' @return NULL
+#' 
+#' @author rnahf
+#' @export
+setGeneric("setPublicAccess", function(x, ...) {
+  standardGeneric("setPublicAccess")
+})
+
+
+setMethod("setPublicAccess", signature("D1Object"), function(x) {
+    jD1Object = x@jD1o
+	if(!is.jnull(jD1Object)) {
+		
+		jPolicyEditor <- jD1Object$getAccessPolicyEditor()
+		if (!is.jnull(jPolicyEditor)) {
+			message("setPublicAccess: got policy editor")
+			jPolicyEditor$setPublicAccess()
+		} else {
+			print("policy editor is null")
+		}
+	}
+})
+
+
+
+#' Test whether the provided subject can read the object
+#' 
+#' Using the AccessPolicy, tests whether the subject has read permission
+#' for the object.  This method is meant work prior to submission, so uses
+#' only the AccessPolicy to determine who can read (Not the rightsHolder field,
+#' which always can read.)
+#' @param x D1Client
+#' @param subject : character
+#' @param ... (not yet used)
+#' @returnType logical
+#' @return TRUE or FALSE
+#' 
+#' @author rnahf
+#' @export
+setGeneric("canRead", function(x, subject, ...) {
+  standardGeneric("canRead")
+})
+
+## TODO: is this method really necessary? Can it be implemented more fully? (looking at rightsHolder)
+## (want to be able to work prior to submission)
+setMethod("canRead", signature("D1Object", "character"), function(x, subject) {
+    jD1Object = x@jD1o
+	if(!is.jnull(jD1Object)) {
+		jPolicyEditor <- jD1Object$getAccessPolicyEditor()
+		if (!is.jnull(jPolicyEditor)) {
+			message("canRead: got policy editor")
+			jSubject <- J("org/dataone/client/D1TypeBuilder", "buildSubject", subject)
+			jPermission <- J("org/dataone/service/types/v1/Permission", "convert", "read")
+			result <- jPolicyEditor$hasAccess(jSubject,jPermission)
+		} else {
+			print("policy editor is null")
+			result <- FALSE
+		}
+	}
+	return(result)
+})
+
+
+
+setGeneric("asDataFrame", function(x, reference, ...) { 
+            standardGeneric("asDataFrame")
+        })
+
+##
+## this method uses the provided metadata reference object for instructions on
+## how to parse the data table (which parameters to set)
+## 'reference' is the metadata D1Object that gives instruction on how to read the data
+## into the dataFrame
+##
+## @rdname asDataFrame-methods
+## aliases asDataFrame,D1Object,D1Object-method
+setMethod("asDataFrame", signature("D1Object", "D1Object"), function(x, reference, ...) {
+            ## reference is a metadata D1Object
+            mdFormat <- getFormatId(reference)
+            
+            dtdClassName <- tableDescriber.registry[[ mdFormat ]]
+            message(paste("@@ asDataFrame/Object", getIdentifier(reference), dtdClassName))
+            if (!is.na(dtdClassName)) {
+                dtd <-	do.call(dtdClassName, list(reference))
+                df <- asDataFrame(x,dtd)
+            } else {
+                print("Could not find metadata parser, trying as plain csv...")
+                df <-  asDataFrame(x)
+            }
+            return( df )
+        })
+
+
+## @rdname asDataFrame-methods
+## aliases asDataFrame,D1Object,AbstractTableDescriber
+setMethod("asDataFrame", signature("D1Object", "AbstractTableDescriber"), function(x, reference, ...) {
+            
+            message("asDataFrame / D1Object-dtd",class(reference))
+            ## reference is a TableDescriber
+            pids <- documented.d1Identifiers(reference)
+            jDataId <- x@jD1o$getIdentifier()$getValue()
+            index <- which(pids == jDataId)
+            message(paste("Index of data item is",index))
+            
+            ## is this a datatype that we can handle?
+			## trust the metadata, not the d1FormatId of the object
+            dataFormat <- data.formatFamily(reference,index)
+            if (dataFormat != "text/simpleDelimited") {
+				message("cannot process data of type", dataFormat)
+                return()
+            } else if (data.tableAttributeOrientation(reference, index) == 'row') {
+				message("cannot process text/simpleDelimited file where attributes are by row")
+            }
+            
+            fieldSeparator <- data.tableFieldDelimiter(reference, index)
+            if (is.na(fieldSeparator))
+                fieldSeparator <- ","
+            
+            quoteChar <- data.tableQuoteCharacter(reference, index)
+            if (is.na(quoteChar))
+                quotChar <- "\""
+            
+            missingValues <- data.tableMissingValueCodes(reference,index)
+            missingValues <- subset(missingValues, !is.na(missingValues))
+            if(length(missingValues)==0)
+                missingValues <- "NA"
+            
+            encoding <- data.characterEncoding(reference, index)
+            if (is.na(encoding))
+                encoding <- "unknown"
+            
+            skip <- data.tableSkipLinesHeader(reference, index)
+            if (is.na(skip))
+                skip <- 0
+            
+            
+            ## TODO: add the colClasses logic
+            
+            ## as.is = !stringsAsFactors,
+            ##  colClasses = NA, nrows = -1,
+            ## check.names = TRUE, 
+            ## fill = !blank.lines.skip,
+            ## strip.white = FALSE, 
+            ## blank.lines.skip = TRUE,
+            ## comment.char = "#",
+            ## allowEscapes = FALSE, flush = FALSE,
+            ## stringsAsFactors = default.stringsAsFactors(),
+            message("@@ skip ",skip)
+            message("@@ sep ",fieldSeparator)
+            message("@@ quote ",quoteChar)
+            message("@@ na.strings ",missingValues)
+            message("@@ encoding ",encoding)
+            df <- asDataFrame(x, skip=skip, header=TRUE, sep=fieldSeparator, quote=quoteChar, 
+                    na.strings=missingValues, encoding=encoding)
+            return(df)
+        })
+
+##
+##  this method performs a read.csv on the D1Object data.  As with read.csv, you 
+##  can use any of the parameters from read.table to override default behavior 
+##  (see read.csv and read.table)
+##
+## @rdname asDataFrame-methods
+## aliases asDataFrame,D1Object,ANY-method
+setMethod("asDataFrame", signature("D1Object"), function(x, ...) {
+            ## Load the data into a dataframe
+            
+            dataBytes <- getData(x)
+            theData <- textConnection(dataBytes)
+            message("theData is ", class(theData))
+            ## using read.csv instead of read.table, because it exposes the defaults we want
+            ## while also allowing them to be overriden
+            df <- read.csv(theData, ...)
+            return(df)
+        })
+
+
---
+++ R/DataPackage.R
@@ -0,0 +1,297 @@
+#
+#   This work was created by participants in the DataONE project, and is
+#   jointly copyrighted by participating institutions in DataONE. For
+#   more information on DataONE, see our web site at http://dataone.org.
+#
+#     Copyright 2011-2014
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+setClass("DataPackage",
+         representation(packageId = "character",
+                        jDataPackage = "jobjRef")
+)
+
+###########################
+## DataPackage constructors
+###########################
+
+## generic
+setGeneric("DataPackage", function(...) { standardGeneric("DataPackage")} )
+##setGeneric("DataPackage", function(identifier, jDataPackage, ...) {
+##standardGeneric("DataPackage")
+##})
+
+
+setMethod("initialize", "DataPackage", function(.Object, packageId, jDataPackage) {
+	message("@@ DataPackage-class.R initialize")
+	## verify the jDataPackage
+	if (missing("jDataPackage")) {
+        message("@@ DataPackage-class.R - (missing jDataPackage)...")
+		if (!missing("packageId")) {
+            message("@@ DataPackage-class.R - (have packageId)...")
+			## set the packageId and instantiate a new jDataPackage
+			.Object@packageId <- packageId
+			jPid <- .jnew("org/dataone/service/types/v1/Identifier")
+			jPid$setValue(packageId)
+			jDataPackage <- .jnew("org/dataone/client/DataPackage",jPid, check=FALSE)
+			if (!is.null(e <- .jgetEx())) {
+				print("Java exception was raised")
+				print(.jcheck(silent=TRUE))
+				print(.jcheck(silent=TRUE))
+				print(e)
+				jDataPackage <- .jnull("org/dataone/client/DataPackage")
+			}
+			.Object@jDataPackage <- jDataPackage
+		}
+	} else {
+        message("@@ DataPackage-class.R - (have a jDataPackage...)")
+		## check that jDataPackage is indeed one
+		## then set the packageId from that one
+		if (.jinstanceof(jDataPackage,"org/dataone/client/DataPackage")) {
+            message("@@ DataPackage-class.R - (jDataPackage is a DataPackage instance)...")
+			jPid <- jDataPackage$getPackageId()
+			.Object@packageId <- jPid$getValue()
+			.Object@jDataPackage <- jDataPackage
+		}
+	}
+   return(.Object)
+})
+
+
+##  getData returns the actual data of an object contained in the package
+##  given the identifier string
+setMethod("getData", signature("DataPackage", "character"), function(x, id) {
+  
+  jIdentifier <- .jnew("org/dataone/service/types/v1/Identifier")
+  jIdentifier$setValue(id)
+
+  jD1Object <- x@jDataPackage$get(jIdentifier)
+  if(!is.jnull(jD1Object)) {
+    databytes <- jD1Object$getData()
+    if(is.null(databytes)) {
+      print(paste("Didn't find data in object", id))
+      return()
+    }
+    return(databytes)
+  }
+})
+
+
+
+
+## Get the Count of Objects in the Package
+## @param x DataPackage
+## @param ... (not yet used)
+## @returnType numeric
+## @return the number of object in the Package
+## 
+## @author rnahf
+## @export
+setGeneric("getSize", function(x, ...) { standardGeneric("getSize")} )
+
+setMethod("getSize", "DataPackage", function(x) {
+  return(x@jDataPackage$size())
+})
+
+
+
+## Get the Identifiers of Package Members
+## 
+## Return the identifiers of the package members, as defined by the ResourceMap
+## @param x : DataPackage
+## @param ... (not yet used)
+## @returnType character
+## @return list of identifiers
+## 
+## @author rnahf
+## @export
+setGeneric("getIdentifiers", function(x, ...) { standardGeneric("getIdentifiers")} )
+
+setMethod("getIdentifiers", "DataPackage", function(x) {
+  jSet <- x@jDataPackage$identifiers()
+  identifiers <- character(0)
+  jIt <- jSet$iterator()
+  while(jIt$hasNext()) {
+    jPid <- .jrcall(jIt,"next")
+    identifiers <- append(identifiers, jPid$getValue())
+  }
+  return(identifiers)
+})
+
+
+
+
+## Add a D1Object to the DataPackage
+## 
+## Includes the D1Object in the DataPackage data Map, making it available for
+## retrieval and eventual upload (via createPackage)
+## 
+## @param x : DataPackage
+## @param d1object : D1Object
+## @param ... : (not yet used)
+## 
+## @author rnahf
+## @export
+setGeneric("addData", function(x, d1object, ...) { 
+    standardGeneric("addData")
+})
+
+setMethod("addData", signature("DataPackage", "D1Object"), function(x, d1object) {
+  jD1object <- d1object@jD1o
+  x@jDataPackage$addData(jD1object)
+})
+
+
+
+
+## Add a Pre-Existing Object to the Package
+## 
+## addAndDownloadData downloads a D1Object to the DataPackage, using the provided identifier
+## string to retrieve from the DataONE system.
+## 
+## @param x : DataPackage
+## @param identifier : character - the identifier of the object to act upon
+## @param ... (not yet used)
+## 
+## @author rnahf
+## @export
+setGeneric("addAndDownloadData", function(x, identifier, ...) { 
+    standardGeneric("addAndDownloadData")
+})
+
+setMethod("addAndDownloadData", signature("DataPackage", "character"), function(x, identifier) {
+  jPid <- .jnew("org/dataone/service/types/v1/Identifier")
+  jPid$setValue(identifier)
+  x@jDataPackage$addAndDownloadData(jPid)
+})
+
+
+
+
+## Associates Data Objects to the Science Metadata Objects that Describe Them
+## 
+## @note Since the resource map that defines a package is separate from the items
+## it associates, it is possible to use identifiers that have not been defined 
+## as members of the package.
+setGeneric("insertRelationship", function(x, subjectID, objectIDs, predicateNS, predicateURI, ...) {
+  standardGeneric("insertRelationship")
+})
+
+
+setMethod("insertRelationship", signature("DataPackage", "character", "character"), function(x, subjectID, objectIDs, ...) {
+  jMetaPid <- .jnew("org/dataone/service/types/v1/Identifier")
+  jMetaPid$setValue(subjectID)
+  
+  jList <- .jnew("java/util/LinkedList")
+  for (id in objectIDs) {
+    jPid <- .jnew("org/dataone/service/types/v1/Identifier")
+    jPid$setValue(id)
+    jList$add(jPid)
+  }
+  x@jDataPackage$insertRelationship(jMetaPid, jList)
+  if (!is.jnull(e <- .jgetEx())) {
+      print("    ** Java exception was raised")
+      print(.jcheck(silent=FALSE))
+  }
+})
+
+setMethod("insertRelationship", signature("DataPackage", "character", "character", "character", "character"), function(x, subjectID, objectIDs, predicateNS, predicateURI) {
+  jSubjPid <- .jnew("org/dataone/service/types/v1/Identifier")
+  jSubjPid$setValue(subjectID)
+  
+  jList <- .jnew("java/util/LinkedList")
+  for (id in objectIDs) {
+    jPid <- .jnew("org/dataone/service/types/v1/Identifier")
+    jPid$setValue(id)
+    jList$add(jPid)
+  }
+  x@jDataPackage$insertRelationship(jSubjPid, jList, predicateNS, predicateURI)
+  if (!is.jnull(e <- .jgetEx())) {
+    print("    ** Java exception was raised")
+    print(.jcheck(silent=FALSE))
+  }
+})
+
+## Returns true if the specified object is a member of the package
+##  
+setGeneric("contains", function(x, identifier, ...) {
+  standardGeneric("contains")
+})
+
+setMethod("contains", signature("DataPackage", "character"), function(x, identifier) {
+  jPid <- .jnew("org/dataone/service/types/v1/Identifier")
+  jPid$setValue(identifier)
+  
+  return(x@jDataPackage$contains(jPid))
+})
+
+
+## Remove the Specified Member from the Package
+## 
+## Given the identifier of a member of the data package, return the D1Object
+## representation of the member.
+## 
+setGeneric("removeMember", function(x, identifier, ...) {
+  standardGeneric("removeMember")
+})
+
+setMethod("removeMember", signature("DataPackage", "character"), function(x, identifier) {
+  jPid <- .jnew("org/dataone/service/types/v1/Identifier")
+  jPid$setValue(identifier)
+  
+  x@jDataPackage$remove(jPid)
+})
+
+
+## Return the Package Member by Identifier
+## 
+## Given the identifier of a member of the data package, return the D1Object
+## representation of the member.
+## 
+setGeneric("getMember", function(x, identifier, ...) {
+  standardGeneric("getMember")
+})
+
+setMethod("getMember", signature("DataPackage", "character"), function(x, identifier) {
+  jPid <- .jnew("org/dataone/service/types/v1/Identifier")
+  jPid$setValue(identifier)
+  
+  jD1Object <- x@jDataPackage$get(jPid)
+  rD1o <- new(Class="D1Object",jD1Object)
+  return(rD1o)
+})
+
+
+## returns a DataFrame from the specified data object
+## 
+## Given the identifier of a member of the data package, return a data frame
+## using any parsing instructions contained in its describing science metadata
+## 
+## @rdname asDataFrame-methods
+## @aliases asDataFrame,DataPackage,character-method
+setMethod("asDataFrame", signature("DataPackage", "character"), function(x, reference) {
+            
+            ## find the dataObject and the metadata that Documents it
+            d1o <- getMember(x,reference)
+            jMetadataId <- x@jDataPackage$getDocumentedBy(d1o@jD1o$getIdentifier())
+            documenterObject <- getMember(x,jMetadataId$getValue())
+            message("@@ asDataFrame / DP")
+            ##	message(documenterObject)
+            df <- asDataFrame(d1o,documenterObject)
+            return(df)
+        })
+
+
+
