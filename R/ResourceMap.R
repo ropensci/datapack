3a4a19e30cce22792fcc3f3268805696944f7823
sbpcs59 <slaughter.nceas.ucsb.edu>
2016-03-21 14:28:38 -0700

    addition update for insertRelationship returning DataPackage

--- R/ResourceMap.R
+++ R/ResourceMap.R
@@ -32,7 +32,7 @@
 #' @keywords resourceMap
 #' @examples
 #' dp <- new("DataPackage")
-#' insertRelationship(dp, "/Users/smith/scripts/genFields.R",
+#' dp <- insertRelationship(dp, "/Users/smith/scripts/genFields.R",
 #'     "http://www.w3.org/ns/prov#used",
 #'     "https://knb.ecoinformatics.org/knb/d1/mn/v1/object/doi:1234/_030MXTI009R00_20030812.40.1")
 #' relations <- getRelationships(dp)
