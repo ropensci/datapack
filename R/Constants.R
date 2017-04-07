e6f22a09a3218fc41b7e031e5e6bf987f2d20b1c
gothub <slaughter@nceas.ucsb.edu>
2017-04-07 10:54:49 -0700

    Add foaf and cito namespaces

--- R/Constants.R
+++ R/Constants.R
@@ -9,6 +9,8 @@
 xsdStringURI             <- "http://www.w3.org/2001/XMLSchema#string"
 DCidentifier             <- "http://purl.org/dc/terms/identifier"
 RDF_NS                   <- "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+FOAF_NS                  <- "http://xmlns.com/foaf/0.1/"
+foafName                 <- sprintf("%s%s", FOAF_NS, "name")
 rdfType                  <- sprintf("%s%s", RDF_NS, "type")
 provNS                   <- "http://www.w3.org/ns/prov#"
 provQualifiedAssociation <- sprintf("%s%s", provNS, "qualifiedAssociation")
@@ -27,6 +29,9 @@ provONEuser              <- sprintf("%s%s", provONE_NS, "User")
 xsdString                <- sprintf("http://www.w3.org/2001/XMLSchema#string")
 D1_CN_URL                <- "https://cn.dataone.org/cn/v2"
 D1_CN_Resolve_URL        <- sprintf("%s/%s", D1_CN_URL, "resolve")
+cito_NS                  <- "http://purl.org/spar/cito/"
+citoDocuments            <-  sprintf("%s%s", cito_NS, "documents")
+citoIsDocumentedBy       <-  sprintf("%s%s", cito_NS, "isDocumentedBy")
 
 knownNamespaces <- data.frame(namespace=character(), prefix=character(), stringsAsFactors=FALSE)
 knownNamespaces <- rbind(knownNamespaces, data.frame(namespace=RDF_NS, prefix="rdf", row.names = NULL, stringsAsFactors = FALSE))
