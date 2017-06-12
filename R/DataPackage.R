8bf826a88b8d4d8f22dd870189b2c22330ca0b12
gothub <slaughter@nceas.ucsb.edu>
2017-06-12 12:38:11 -0700

    add DataPackage setPublicAccess()

--- R/DataPackage.R
+++ R/DataPackage.R
@@ -1084,18 +1084,27 @@ setMethod("getValue", signature("DataPackage"), function(x, name, identifiers=as
 
 #' @rdname setPublicAccess
 #' @aliases setPublicAccess
-#' @param identifiers A list of \code{character} values containing package member identifiers that will be updated.
+#' @param identifiers A list of \code{character} values containing package member identifiers that will be updated (default is all package members).
 #' @examples
+#' # First create a sample package with two DataObjects
+#' dp <- new("DataPackage")
 #' data <- charToRaw("1,2,3\n4,5,6\n")
-#' do <- new("DataObject", "id1", dataobj=data, "text/csv", 
-#'   "uid=jones,DC=example,DC=com", "urn:node:KNB")
-#' do <- setPublicAccess(do)
+#' obj <- new("DataObject", id="id1", dataobj=data, format="text/csv")
+#' dp <- addMember(dp, obj)
+#' data2 <- charToRaw("7,8,9\n4,10,11\n")
+#' obj2 <- new("DataObject", id="id2", dataobj=data2, format="text/csv")
+#' dp <- addMember(dp, obj2)
+#' # Now add public read to all package members ("id1", "id2")
+#' dp <- setPublicAccess(dp)
 setMethod("setPublicAccess", signature("DataPackage"), function(x, identifiers=list()) {
-    matchingIds <- list()
+    if(length(identifiers) == 0) identifiers <- getIdentifiers(x)
     if(length(keys(x@objects)) > 0) {
         for(iKey in keys(x@objects)) {
             if(! iKey %in% identifiers) next
-            setPublicAccess(x@objects[[iKey]])
+            obj <- getMember(x, identifier=iKey)
+            obj <-  setPublicAccess(obj)
+            x <- removeMember(x, obj, keepRelationships=TRUE)
+            x <- addMember(x, obj)
         }
     } else {
         stop("The specified package has no members")
